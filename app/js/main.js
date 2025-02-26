849bc295e3b34cf1d8824d69cddd20cf16e2bfcd
Iryna Kosovan <kosovaniryne@gmail.com>
2025-02-26 21:47:21 +0200

    created all css properties to sections and added scroll plugin

--- app/js/main.js
+++ app/js/main.js
@@ -4,4 +4,15 @@ $(function() {
     prevArrow:'<button class="about__btn about__btn-left"><svg width="17" height="9" viewBox="0 0 17 9" fill="none" xmlns="http://www.w3.org/2000/svg"><g opacity="0.64"></g><path d="M1 1L8.5 8L16 1"/></svg></button>',
     nextArrow: '<button class="about__btn about__btn-right"><svg width="17" height="9" viewBox="0 0 17 9" fill="none" xmlns="http://www.w3.org/2000/svg"><g opacity="0.64"></g><path d="M1 1L8.5 8L16 1"/></svg></button>'
   })
+
+  $('.questions__subtitle').on('click', function() {
+    $('.questions__item').removeClass('questions__item--active');
+    $(this).parent().addClass('questions__item--active')
+  })
+
+  $('#fullpage').fullpage({
+    autoScrolling:true,
+    scrollHorizontally: true,
+    sectionSelector: '.page-section',
+});
 })
